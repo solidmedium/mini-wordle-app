b5c50b75199070f191cff351962857867ce7795e
Davin McDonald <davin@solidmedium.com.au>
2025-03-26 16:19:00 +1100

    Add shadcn, add niver UI and styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 "use client"
 
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
 import React, { useState } from 'react';
 
 export default function Home() {
@@ -7,20 +9,26 @@ export default function Home() {
   // const [answer, setAnswer] = useState<string>('TABLE');
   const [guess, setGuess] = useState<string>('');
   const [wordArray, setWordArray] = useState<string[]>([]);
+  const [errorMessage, setErrorMessage] = useState<string>('');
 
   const handleOnchange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setGuess(e.target.value);
   };
 
   const handleSubmit = () => {
-    setWordArray([guess, ...wordArray]);
+    if (guess.length !== 5) {
+      setErrorMessage('Please enter 5 characters');
+      return;
+    }
+    setErrorMessage('');
+    setWordArray([guess.toLocaleUpperCase(), ...wordArray]);
     setGuess('');
   };
 
   const RenderRows = () => {
     return wordArray.map((row, index) => {
       return (
-        <div key={index}>
+        <div className="flex space-2 my-5 justify-between" key={index}>
           <RenderWords  word={row} />
         </div>
       );
@@ -33,20 +41,25 @@ export default function Home() {
     
     return splitWords.map((value, index) => {
       let isMatch = ''
-      if (splitAnswer.includes(value)) isMatch = 'yellow'
+      if (splitAnswer.includes(value)) isMatch = 'bg-yellow-300'
       splitAnswer.map((ansVal, ind) => {
-        if(ansVal === value && ind === index) isMatch = 'green'
+        if(ansVal === value && ind === index) isMatch = 'bg-green-600'
       })
-      return <span style={{ backgroundColor: isMatch }} key={index}>{value}</span>;
+      return <div className={`border border-slate-300 font-bold rounded-lg flex justify-center items-center w-[50px] h-[50px] ${isMatch}`} key={index}>{value}</div>;
     });
   };
 
   return (
-    <div>
-      <p>Guess the word</p>
-      <input onChange={(e) => handleOnchange(e)} maxLength={5} value={guess} />
-      <button onClick={() => handleSubmit()}>Submit</button>
-      <RenderRows />
+    <div className="flex items-center justify-center h-screen bg-slate-100">
+      <div className="max-w-[400px] min-h-[500px] mx-auto p-4">
+        <h1 className="text-4xl mb-3">Guess the word</h1>
+        <div className="flex w-full max-w-sm items-center space-x-2">
+          <Input onChange={(e) => handleOnchange(e)} maxLength={5} minLength={5} value={guess} className="uppercase" />
+          <Button onClick={() => handleSubmit()}>Submit</Button>
+        </div>
+        {errorMessage && <p className="text-red-500 my-3">{errorMessage}</p>}
+        <RenderRows />
+      </div>
     </div>
   );
 }
